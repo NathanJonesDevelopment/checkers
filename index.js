99f062b90455e278cdad24a2ea0e17ca8cfd09c0
Nathan Jones <nmjones@live.com>
2017-03-21 07:21:21 +1000

    server now serves files from different location in production

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ var app = express();
 var http = require('http').Server(app);
 var path = require('path');
 
-app.use(express.static(__dirname + '/build'))
+//app.use(express.static(__dirname + '/build'))
+
+if (__dirname.slice(-6) === '/build') { // For production
+	app.use(express.static(__dirname))
+} else { // For development
+	app.use(express.static(__dirname + '/build'))
+}
 
 http.listen(process.env.PORT || 3000, function(){
 	console.log(`listening on port: ${process.env.PORT || '3000'}`);

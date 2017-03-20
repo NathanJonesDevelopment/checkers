7cafe759deee3e66473801a32c6b3204f243a8ed
Nathan Jones <nmjones@live.com>
2017-03-20 20:29:44 +1000

    more server updates

--- build/index.js
+++ build/index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/build/index.html');
 });
 
 http.listen(process.env.PORT || 3000, function(){
--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var path = require('path');
 
-/*
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/build');
-});
-*/
-
-app.use(express.static(__dirname + '/build'))
+app.use(express.static(path.join(__dirname + '/build')))
 
 http.listen(process.env.PORT || 3000, function(){
-	console.log('listening on *:3000');
+	console.log(`listening on port: ${process.env.PORT || '3000'}`);
 });

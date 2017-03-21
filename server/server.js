10ba37e83af8840224814bd620ad64dffc1fe9f1
Nathan Jones <nmjones@live.com>
2017-03-21 13:30:48 +1000

    changed location of server file and changed npm start command

--- server/server.js
+++ server/server.js
@@ -3,13 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var path = require('path');
 
-//app.use(express.static(__dirname + '/build'))
-
-if (__dirname.slice(-6) === '/build') { // For production
-	app.use(express.static(__dirname))
-} else { // For development
-	app.use(express.static(__dirname + '/build'))
-}
+app.use(express.static(path.join(__dirname + '/../build')))
 
 http.listen(process.env.PORT || 3000, function(){
 	console.log(`listening on port: ${process.env.PORT || '3000'}`);

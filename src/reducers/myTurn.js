77feeaba275c230ff96be03704af044a1db8dc48
Nathan Jones <nmjones@live.com>
2017-04-10 15:23:48 +1000

    removed unused files

--- src/reducers/myTurn.js
+++
@@ -1,3 +0,0 @@
-import { Reducer } from 'react-redux-utilities'
-
-export default Reducer(true, {toggleMyTurn: (state, action) => !state})
--- src/reducers/playersReady.js
+++
@@ -1,5 +0,0 @@
-import { Reducer } from 'react-redux-utilities'
-
-export default Reducer(false, {
-    playersReady: (state, action) => true
-})
